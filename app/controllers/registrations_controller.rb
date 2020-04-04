5e6a05cac7b441ec689e5d66823d2516f28b17e2
Paul Gaumer <mitsudomoe8@gmail.com>
2020-04-04 15:44:04 +0200

    Add Podcast instance creation on user sign up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+      super
+      if resource.persisted?
+        Rails.logger.info("Just created and saved #{resource}");
+        p = Podcast.new(user: resource)
+        p.subdomain = params[:subdomain]
+        p.save!
+      end
+  end
+
+  # private
+
+  # # Notice the name of the method
+  # def sign_up_params
+  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  # end
+end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,5 +1,5 @@
 class Podcast < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true
+  validates :subdomain, presence: true, uniqueness: true
 end
