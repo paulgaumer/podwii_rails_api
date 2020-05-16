8147387f861d1196a5ba7f3cbc0e53ae1efe7792
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-16 15:14:46 +0200

    Fix subdomain issue

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,6 +3,7 @@ class Podcast < ApplicationRecord
   has_many :episodes, dependent: :destroy
   has_many :crm_items, dependent: :destroy
   has_many :themes, dependent: :destroy
+  # has_many :subdomains, dependent: :destroy
 
   validates :subdomain, presence: true, uniqueness: true
 end
