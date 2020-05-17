33197dfbf6d19c4b226406ca91acbd5889468875
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-17 12:31:45 +0200

    Fix Heroku bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
       Rails.logger.info("Just created and saved #{resource}")
       p = Podcast.new(user: resource)
       p.subdomain = params[:subdomain]
-      p.themes.new(color: { "primary" => "#F97F7F", "headerText" => "#D17C78", "headerBackground" => "#181D46" })
+      p.themes.new(colors: { "primary" => "#F97F7F", "headerText" => "#D17C78", "headerBackground" => "#181D46" })
       p.save!
     end
   end
