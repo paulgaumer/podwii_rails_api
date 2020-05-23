b053b779d049c5952f6fefdb9bf0aafbe235649d
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-23 17:59:45 +0200

    Pundit issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,30 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
-    super
+    build_resource(sign_up_params)
+    resource.save
+    yield resource if block_given?
     if resource.persisted?
       Rails.logger.info("Just created and saved #{resource}")
       p = Podcast.new(user: resource)
       p.subdomain = params[:subdomain]
-      p.themes.new(colors: { "primary" => "#F97F7F", "headerText" => "#D17C78", "headerBackground" => "#181D46" }, "activeTheme" => "theme1")
-      p.save!
+      p.themes.new(colors: { "primary" => "#F97F7F", "headerText" => "#D17C78", "headerBackground" => "#181D46", "activeTheme" => "theme1" })
+      if p.save
+        render json: { message: "User & Podcast created" }
+      else
+        resource.destroy
+        render json: { errors: p.errors.full_messages },
+               status: :unprocessable_entity
+      end
+    else
+      render json: { errors: resource.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
-  # private
+  private
 
   # # Notice the name of the method
-  # def sign_up_params
-  #   params.require(:user).permit(:email, :password, :password_confirmation)
-  # end
+  def sign_up_params
+    params.require(:user).permit(:email, :password)
+  end
 end
