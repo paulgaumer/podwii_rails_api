843e5621e79d36b21755e7f60b9409d4b4d8c881
Paul Gaumer <mitsudomoe8@gmail.com>
2020-05-23 18:00:48 +0200

    fix activeTheme

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
       Rails.logger.info("Just created and saved #{resource}")
       p = Podcast.new(user: resource)
       p.subdomain = params[:subdomain]
-      p.themes.new(colors: { "primary" => "#F97F7F", "headerText" => "#D17C78", "headerBackground" => "#181D46" }, "activeTheme" => "theme1")
+      p.themes.new(colors: { "primary" => "#F97F7F", "headerText" => "#D17C78", "headerBackground" => "#181D46", "activeTheme" => "theme1" })
       p.save!
     end
   end
