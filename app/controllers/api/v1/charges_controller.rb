f4154b6965676e54f78c16f0dbfae9811f116c7c
Paul Gaumer <mitsudomoe8@gmail.com>
2020-08-18 17:19:43 +0200

    Payment works for subscription

--- app/controllers/api/v1/charges_controller.rb
+++ app/controllers/api/v1/charges_controller.rb
@@ -4,14 +4,26 @@ class Api::V1::ChargesController < ActionController::API
   def create
     Stripe.api_key = "sk_test_WufmgV5wuW7Rxm7mG8bzSQ3600BtXV2VQL"
 
-    payment_intent = Stripe::PaymentIntent.create(
-      amount: 6500,
-      currency: "usd",
+    # payment_intent = Stripe::PaymentIntent.create(
+    #   amount: 6500,
+    #   currency: "usd",
+    # )
+    # render json: { clientSecret: payment_intent["client_secret"] }
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      line_items: [{
+        price: "price_1HGmpwCFTLTHD2lH4SoiqDVo",
+        quantity: 1,
+      }],
+      subscription_data: {
+        trial_from_plan: true,
+      },
+      mode: "subscription",
+      success_url: "https://paulgaumer.com?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "https://japanlifestories.com",
     )
-    # {
-    #   clientSecret: payment_intent["client_secret"],
-    # }.to_json
-    render json: { clientSecret: payment_intent["client_secret"] }
+    render json: { sessionId: session.id }
   rescue Stripe::CardError => e
     render json: { message: e.message }, status: :not_acceptable
   end
