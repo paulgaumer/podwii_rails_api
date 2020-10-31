aae1ef2408674963525d358715961a0b524fb9b4
Paul Gaumer <mitsudomoe8@gmail.com>
2020-10-31 16:56:53 +0100

    fix: slack webhook error on user creation. Regenerate a new webhook url and move it to env variable

--- app/services/notifications/slack_notifier.rb
+++ app/services/notifications/slack_notifier.rb
@@ -2,7 +2,7 @@ require "slack-notifier"
 
 class Notifications::SlackNotifier
   def self.call(email, podcast)
-    url = "https://hooks.slack.com/services/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"
+    url = ENV["SLACK_WEBHOOK_URL"]
     notifier = Slack::Notifier.new(url)
     notifier.ping "🎉 New user: #{email}, Podcast: [#{podcast}] 🎉"
   end
